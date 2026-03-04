c24c494e53b0ba78fcf3ffc72b5bc130da3ef048
Sdkrsn <sdkry0110@gmail.com>
2026-03-04 11:31:33 +0530

    Add

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,11 +8,11 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Siddharth Radhakrishnan</h1>
-        <p className={styles.description}>I am a third-year B.Tech Computer Science and Engineering (CSE) with IoT student, passionate about Machine Learning (ML) and its applications. 
+        <p className={styles.description}>I am a fourth-year B.Tech Computer Science and Engineering (CSE) with IoT student, passionate about Machine Learning (ML) and its applications. 
         Additionally, I have gained experience in ethical hacking, as well as web and app development, and I am eager to explore innovative projects in these domains.
 
         </p>
-        <a href="/Sid_resume.pdf" download="Siddharth_Resume" className={styles.contactBtn}>
+        <a href="/Siddharth_CV(R).pdf" download="Siddharth_Resume" className={styles.contactBtn}>
           Download Resume
         </a>
       </div>
